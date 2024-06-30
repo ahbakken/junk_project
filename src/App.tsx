157fb323392c72279832042adbc7fbdfff137d0a
Astrid Bakken <ahbakken@icloud.com>
2024-06-30 12:22:15 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, AppShell, Center } from "@mantine/core";
 import { theme } from "./theme";
 import "./App.css";
 import CalculatorSection from "./sections/calculator";
+import JunkSection from "./sections/junk";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
+import Home from "./sections/home";
 
 export default function App() {
+  const [opened] = useDisclosure();
+
   return (
     <MantineProvider theme={theme}>
-      <div className="app-content">
-        <CalculatorSection />
-      </div>
+      <BrowserRouter>
+        <AppShell
+          title="Junk Project"
+          withBorder
+          padding={"md"}
+          header={{ height: 60 }}
+          navbar={{
+            width: 300,
+            breakpoint: "sm",
+            collapsed: { mobile: !opened },
+          }}
+        >
+          <AppShell.Header>
+            <div>Logo</div>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/calculator">Calculator</Link>
+              <Link to="/junk">Junk</Link>
+            </nav>
+          </AppShell.Header>
+          <Center>
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="/calculator" Component={CalculatorSection} />
+              <Route path="/junk" Component={JunkSection} />
+            </Routes>
+          </Center>
+        </AppShell>
+      </BrowserRouter>
     </MantineProvider>
   );
 }
